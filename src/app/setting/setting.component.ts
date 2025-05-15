c7d7fe848393952c2747d505cc12f75375d7347c
Pittawan <chantrarut_p@silpakorn.edu>
2025-05-15 17:32:37 +0700

    filter & take

--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -1,7 +1,25 @@
-import { Component } from "@angular/core";
-
+import { AfterViewInit, Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map, filter, take } from 'rxjs/operators'
 @Component({
   selector: 'app-settings',
-  templateUrl: './setting.component.html'
+  templateUrl: './setting.component.html',
+  styleUrls: ['./setting.component.css']
 })
-export class SettingsComponent { }
+export class SettingsComponent implements AfterViewInit {
+  results: string[] = [];
+
+  ngAfterViewInit() {
+    const input = document.getElementById('myInput') as HTMLInputElement;
+
+    fromEvent(input, 'keyup')
+      .pipe(
+        map(() => input.value.trim()),
+        filter(value => value.length > 3),
+        take(3)
+      )
+      .subscribe(value => {
+        this.results.push(value);
+      });
+  }
+}
